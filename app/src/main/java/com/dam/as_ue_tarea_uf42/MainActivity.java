92602d85743d0b9b0808972f846de8b0f031aed4
deveshaha <devesh_hanumante@outlook.com>
2023-01-30 11:57:46 +0100

    upload project files

---
+++ app/src/main/java/com/dam/as_ue_tarea_uf42/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dam.as_ue_tarea_uf42;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
